5f5b93842f9d07ee93fb8ba4f48cc45665a088e9
Dev Goswami <eng.dev2028@gmail.com>
2026-06-17 20:38:49 +0530

    add promise at checkout page's index.js

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -6,11 +6,25 @@ import { loadProducts } from "../../data/products.js";
 // import "./backendPrectic.js";
 import '../../utils/bootstrap.js';
 
-loadProducts(()=>{
 
-    updateOrderSummary();
+new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve();
+  });
+})
+.then(()=>{
+   updateOrderSummary();
 
     document
       .querySelector(".js-order-summary")
       .addEventListener("click", orderSummaryClickEvent);
 });
+
+// loadProducts(()=>{
+
+//     updateOrderSummary();
+
+//     document
+//       .querySelector(".js-order-summary")
+//       .addEventListener("click", orderSummaryClickEvent);
+// });
